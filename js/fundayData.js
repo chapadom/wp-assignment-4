faf3a33dabdce15d4e381360a65dbe2a93a51924
Mili <mili.chapado@gmail.com>
2019-04-25 18:16:46 -0400

    all

---
+++ js/fundayData.js
@@ -0,0 +1,74 @@
+var tourSites = [
+ {
+   "site": "1",
+   "name": "Giardini Pizza"
+   "address": "363 Smith Street"
+   "type": "optional",
+   "lon": -73.99516225,
+   "lat": 40.67957623
+ },
+ {
+   "site": "2",
+   "name": "Carroll Street F/G Subway Stop",
+   "address": "360 Smith Street",
+   "type": "required",
+   "lon": -73.99580866,
+   "lat": 40.67934028
+ },
+ {
+   "site": "3",
+   "name": "Genius",
+   "address": "92 3rd Street",
+   "type": "required",
+   "lon": -73.99315059,
+   "lat": 40.67739971
+ },
+ {
+   "site": "4",
+   "name": "363 & 365 Bond Street",
+   "address": "",
+   "type": "required",
+   "lon": -73.9898622,
+   "lat": 40.67756041
+ },
+ {
+   "site": "5",
+   "name": "Gowanus Canal Conservatory",
+   "address": "543 Union Street",
+   "type": "required",
+   "lon": -73.98743212,
+   "lat": 40.67927112
+ },
+ {
+   "site": "6",
+   "name": "Spaceworks",
+   "address": "540 President Street",
+   "type": "required",
+   "lon": -73.98542851,
+   "lat": 40.67740988
+ },
+ {
+   "site": "7",
+   "name": "Fifth Avenue Committee",
+   "address": "621 Degraw Street",
+   "type": "required",
+   "lon": -73.9827463,
+   "lat": 40.67901686
+ },
+ {
+   "site": "8",
+   "name": "Ample Hills",
+   "address": "305 Nevins Street",
+   "type": "optional",
+   "lon": -73.9873597,
+   "lat": 40.67893346
+ },
+ {
+   "site": "9",
+   "name": "Pig Beach",
+   "address": "480 Union Street",
+   "type": "optional",
+   "lon": -73.98884296,
+   "lat": 40.67954165
+ }
+]
